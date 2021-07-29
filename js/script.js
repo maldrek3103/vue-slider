ef34715ff8b43ae1f2d45c1bd6e38ffeeac637de
maldrek3103 <cpisanu96@gmail.com>
2021-07-29 16:08:29 +0200

    made html, added imgs with Vue, added style

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 var root = new Vue({
     el: '#root',
     data: {
-        message: 'Hello Vue!'
-    }
+        currentIndex: 0,
+        images: [
+            'images/image1.jpg',
+            'images/image2.jpg',
+            'images/image3.jpg',
+            'images/image4.jpg',
+        ]
+    },
 })
