9f3f00bf2e9237de1e044f7e0a5276dd882794a0
maldrek3103 <cpisanu96@gmail.com>
2021-07-29 16:43:29 +0200

    added logic to display imgs based on index

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+Vue.config.devtools = true;
+
 var root = new Vue({
     el: '#root',
     data: {
@@ -8,5 +10,21 @@ var root = new Vue({
             'images/image3.jpg',
             'images/image4.jpg',
         ]
+    },
+    methods: {
+        isActive(index) {
+            return this.currentIndex === index ? 'active' : '';
+        },
+        increaseIndex() {
+            const lastIndex = this.images.length - 1;
+            this.currentIndex = this.currentIndex === lastIndex ? 0 : this.currentIndex + 1;
+        },
+        decreaseIndex() {
+            const lastIndex = this.images.length - 1;
+            this.currentIndex = this.currentIndex === 0 ? lastIndex : this.currentIndex - 1;
+        },
+        setCurrentIndex(newIndex) {
+            this.currentIndex = newIndex;
+        }
     },
 })
